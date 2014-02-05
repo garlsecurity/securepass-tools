f5603b6b1160fff094517526a0cf72ac3dd0cec1
Giuseppe Paterno <gpaterno@gpaterno.com>
2014-02-05 17:36:29 +0100

    Django experimental backend added

--- README.md
+++ README.md
@@ -4,15 +4,46 @@ SecurePass public CLI tools
 The tools for accessing SecurePass from command line.
 Uses the SecurePass public APIs to manage.
 
-
 More information on the APIs here:
 https://beta.secure-pass.net/
-
 
 
 Configuration file
 ==================
 
-Configuration should be placed in:
+Configuration *only for cli tools* should be placed in:
 /etc/securepass.conf /usr/local/etc/securepass.conf or securepass.conf in current local directory.
 For an example check out securepass.conf.example
+
+
+
+Django Backend
+==============
+
+The Django backend for SecurePass RESTful APIs authenticated and sync information from
+SecurePass (first name, last name, e-mail) each time, so that information is up to date.
+Also state of enable/disable is reflected in is_active, so that the user is automatically disabled.
+
+Note: we do not handle staff at this time. For future that will be reflected into groups.
+
+The following settings have to be put into settings.py
+
+Required:
+SP_APP_ID = <<SecurePass APP ID>>
+SP_APP_SECRET = <<SecurePass APP Secret>>
+
+Optional:
+SP_ENDPOINT = <<endpoint if different from default>>
+SP_AUTOCREATE_USER = <<True/False, autocreate user if not in database>>
+
+
+Put securepass-tools in installed apps
+INSTALLED_APPS += (
+    'securepass-tools',
+)
+
+Put the django authentication backend
+AUTHENTICATION_BACKENDS = (
+    'securepass-tools.djangoauth.SecurePassAuthBackend',
+)
+
---
+++ djangoauth.py
@@ -0,0 +1,71 @@
+##
+## Django SecurePass APIs authentication backend
+##
+## (c) 2013 Giuseppe Paterno' (gpaterno@gpaterno.com)
+##          GARL Sagl (www.garl.ch)
+##
+
+import securepass
+from django.conf import settings
+from django.contrib.auth.models import User
+
+class SecurePassAuthBackend(object):
+
+    """ SecurePass' core authentication backend
+    Settings:
+        SP_APP_ID -> SecurePass APP ID
+        SP_APP_SECRET -> SecurePass APP Secret
+        SP_ENDPOINT -> API endpoint
+        SP_AUTOCREATE_USER -> True/False, set if you want automatic creation of user
+    """
+
+    ## Actual auth
+    def authenticate(self, username, password):
+
+        try:
+            endpoint = settings.SP_ENDPOINT
+            autocreate = settings.SP_AUTOCREATE_USER
+        except:
+            endpoint = "https://beta.secure-pass.net/"
+            autocreate = True
+
+        sp_handle = securepass.SecurePass(app_id=settings.SP_APP_ID,
+                                          app_secret=settings.SP_APP_SECRET,
+                                          endpoint=endpoint)
+
+
+        if sp_handle.user_auth(user=username, secret=password):
+
+            try:
+                mapping = User.objects.get(username=username)
+
+            except User.DoesNotExist:
+                # user will have an "unusable" password
+                if autocreate:
+                    mapping = User.objects.create_user(username, '')
+                    mapping.save()
+                else:
+                    return None
+
+
+            myuser  = sp_handle.user_info(user=username)
+
+            mapping.first_name = myuser['name']
+            mapping.last_name  = myuser['surname']
+            mapping.is_active  = myuser['enabled']
+            mapping.email      = myuser['email']
+
+            mapping.save()
+
+            return mapping
+
+        else:
+            return None
+
+    def get_user(self, user_id):
+        """Retrieve the user's entry in the User model if it exists"""
+
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
