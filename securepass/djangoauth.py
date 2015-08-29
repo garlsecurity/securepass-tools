0908e355312398cfc0a1685f97ac8d16938d6b17
Andrea Grandi <a.grandi@gmail.com>
2015-08-29 11:48:26 +0100

    Fix securepass module import in Django auth module Add realm to the username Fix PEP8 stuff Add missing SP_REALM in docstring Read values from settings in a safer way

--- securepass/djangoauth.py
+++ securepass/djangoauth.py
@@ -1,44 +1,46 @@
-##
-## Django SecurePass APIs authentication backend
-##
-## This code is released under GPLv2
-##
-## (c) 2013 Giuseppe Paterno' (gpaterno@gpaterno.com)
-##          GARL Sagl (www.garl.ch)
-##
+# Django SecurePass APIs authentication backend
+#
+# This code is released under GPLv2
+#
+# (c) 2013 Giuseppe Paterno' (gpaterno@gpaterno.com)
+#          GARL Sagl (www.garl.ch)
+#
 
-from securepass import securepass
+from . import securepass
 from django.conf import settings
 from django.contrib.auth.models import User
+
 
 class SecurePassAuthBackend(object):
 
     """ SecurePass' core authentication backend
     Settings:
-        SP_APP_ID -> SecurePass APP ID
-        SP_APP_SECRET -> SecurePass APP Secret
-        SP_ENDPOINT -> API endpoint
-        SP_AUTOCREATE_USER -> True/False, set if you want automatic creation of user
+        SP_APP_ID          -> SecurePass APP ID
+        SP_APP_SECRET      -> SecurePass APP Secret
+        SP_ENDPOINT        -> API endpoint
+        SP_AUTOCREATE_USER -> True/False, set if you want automatic creation
+                              of user
+        SP_REALM           -> SecurePass realm
     """
 
-    ## Actual auth
+    # Actual auth
     def authenticate(self, username, password):
 
-        try:
-            endpoint = settings.SP_ENDPOINT
-            autocreate = settings.SP_AUTOCREATE_USER
-        except:
-            endpoint = "https://beta.secure-pass.net/"
-            autocreate = True
+        endpoint = getattr(
+            settings, 'SP_ENDPOINT', 'https://beta.secure-pass.net')
+        autocreate = getattr(settings, 'SP_AUTOCREATE_USER', True)
 
-        sp_handle = securepass.SecurePass(app_id=settings.SP_APP_ID,
-                                          app_secret=settings.SP_APP_SECRET,
-                                          endpoint=endpoint)
+        sp_handle = securepass.SecurePass(
+            app_id=getattr(settings, 'SP_APP_ID', ''),
+            app_secret=getattr(settings, 'SP_APP_SECRET', ''),
+            endpoint=endpoint)
 
-
+        # Add realm
+        if '@' not in username:
+            realm = getattr(settings, 'SP_REALM', 'login.farm')
+            username = '{0}@{1}'.format(username, realm)
 
         if sp_handle.user_auth(user=username, secret=password):
-
             try:
                 mapping = User.objects.get(username=username)
 
@@ -51,12 +53,12 @@ class SecurePassAuthBackend(object):
                     return None
 
             try:
-                myuser  = sp_handle.user_info(user=username)
+                myuser = sp_handle.user_info(user=username)
 
                 mapping.first_name = myuser['name']
-                mapping.last_name  = myuser['surname']
-                mapping.is_active  = myuser['enabled']
-                mapping.email      = myuser['email']
+                mapping.last_name = myuser['surname']
+                mapping.is_active = myuser['enabled']
+                mapping.email = myuser['email']
 
                 mapping.save()
             except:
@@ -66,7 +68,6 @@ class SecurePassAuthBackend(object):
 
         else:
             return None
-
 
     def get_user(self, user_id):
         """Retrieve the user's entry in the User model if it exists"""
